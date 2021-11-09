d255a247f7dab7b580695812b85074dbd97aa4a6
Marcos Ochieng <marcosgav80@gmail.com>
2021-11-09 23:43:02 +0300

    iterated through the array and displayed the products with their respective array keys and values

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+// init UI
+const ui = new UI;
+
+
 // iterate through the products array
 products.forEach((product)=>{
-    
+    ui.displayProducts(product.imgSrc,product.title,product.size,product.price);
 })
---
+++ ui.js
@@ -0,0 +1,32 @@
+class UI  {
+    constructor(){
+        this.showProducts = document.querySelector('.menuItems');
+    }
+
+    displayProducts(img,title,size,price){
+        this.showProducts.innerHTML += `
+                        <div class="col products">
+                        <img class="img-fluid" src=${img} alt="pizza">
+                        <h2>${title}</h2>
+                        <h4>Size: ${size}</h4>
+                        <div class="price">
+                        <div>
+                        <h5 class="font-weight-bold">Price: $${price}</h5>
+                        </div>
+                        <div>
+                        <img style="height: 30px;margin-top: -10px;width: 30px;" src="https://img.icons8.com/fluency/48/000000/star.png"/>4.8 (5.9k Review)
+                        </div>
+                        </div>
+                        <form id="form">
+                        <select class="form-select" aria-label="Default select example">
+                        <option selected>Choose topping</option>
+                        <option value="1">Pepperoni</option>
+                        <option value="2">Mushroom</option>
+                        <option value="3">Bacon</option>
+                        </select>
+                        <button class="btn2">Add to cart</button>
+                        </form>
+                    </div>
+        `
+    }
+}
