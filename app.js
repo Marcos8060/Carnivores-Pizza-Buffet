a1513a6380facc7c15e01134b15caae08f547e8b
Marcos Ochieng <marcosgav80@gmail.com>
2021-11-10 10:40:35 +0300

    added items in the cart

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // init UI
 const ui = new UI;
 
-
+const cartItems = document.querySelector('.cartItems');
 // iterate through the products array
 products.forEach((product)=>{
     ui.displayProducts(product.imgSrc,product.title,product.size,product.price,product.id);
@@ -18,8 +18,31 @@ function addToCart(id){
     }else{
         const item = products.find((product)=> product.id === id);
   
-        cart.push(item)
-        console.log(cart);
+        cart.push({
+            ...item,
+            numberOfUnits: 1,
+        })
     }
+    updateCart();
+}
+// update cart
+function updateCart(){
+    appendItemsIncart();
+}
+// append items in cart
+function appendItemsIncart(){
+    cart.forEach((item)=>{
+        cartItems.innerHTML += `
+                <div class="items d-flex justify-content-between align-items-center underline1">
+                <img class="img-fluid" src=${item.imgSrc} alt="pizza" style="width: 60px;">
+                <div class="units d-flex justify-content-between align-items-center">
+                <img src="https://img.icons8.com/ios-glyphs/30/fa314a/plus-math.png"/>
+                <b style="font-size: 25px;">${item.numberOfUnits}</b>
+                <img src="https://img.icons8.com/material/24/fa314a/minus--v2.png"/>
+                </div>
+                <h4>$${item.price}</h4>
+                </div>
+        `
+    })
 }
 
